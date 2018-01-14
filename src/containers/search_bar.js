c48cdd4c15fb0d3b6f9239d85ca5af5ce46206ef
Bill Hilbert <hilbertwilliam@gmail.com>
2018-01-14 16:53:09 -0500

    install axios and redux-promise

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -3,9 +3,7 @@ import React, { Component } from 'react';
 export default class SearchBar extends Component {
 	constructor(props) {
 		super(props);
-
 		this.state = { term: '' };
-
 		this.onInputChange = this.onInputChange.bind(this);
 	}
 
@@ -13,9 +11,15 @@ export default class SearchBar extends Component {
 		this.setState({ term: event.target.value });
 	}
 
+
+	onFormSubmit(event) {
+		event.preventDefault();
+		// We need to fetch weather data
+	}
+
 	render() {
 		return (
-			<form className="input-group">
+			<form onSubmit={this.onFormSubmit} className="input-group">
 				<input 
 					placeholder="Get a five-day forecast in your favorite cities"
 					className="form-control"
